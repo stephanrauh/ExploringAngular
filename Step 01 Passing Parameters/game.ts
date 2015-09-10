9332f9f6415bbe709f6511d66f62e00835366117
stephan <webmaster1@beyondjava.de>
2015-09-10 23:35:58 +0200

    simple demo showing how to pass parameters

---
+++ Step 01 Passing Parameters/game.ts
@@ -0,0 +1,23 @@
+/// <reference path="../typings/angular2/angular2.d.ts" />
+import {Component, View, bootstrap} from 'angular2/angular2';
+import {PlaygroundComponent} from './playground'
+
+
+// Annotation section
+@Component({
+  selector: 'game'
+})
+@View({
+  templateUrl: "game.html",
+  directives: [PlaygroundComponent]
+})
+// Component controller
+class GameComponent {
+  title: string = "initial value";
+  constructor() {
+  }
+}
+
+
+bootstrap(GameComponent);
+console.log("After Bootstrap in app.js")
